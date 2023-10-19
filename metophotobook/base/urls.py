486ee758d7f42ea5235b352ad133a676f7030881
Borsh69 <uludintsevkot@gmail.com>
2023-10-19 20:33:22 +0300

    Тимур можешь начинать делать бекенд

--- metophotobook/base/urls.py
+++ metophotobook/base/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     
     path('', views.home, name="home"),
-
+    path('contest/', views.contest, name="contest"),
+    path('categories/', views.categories, name="categories"),
+    path('user/', views.user, name="user"),
+    path('contests/', views.contests, name="contests"),
 ]
--- metophotobook/base/views.py
+++ metophotobook/base/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base/home.html')
+
+def contest(request):
+    return render(request, 'base/contest-details.html')
+
+def categories(request):
+    return render(request, 'base/categories.html')
+
+
+def user(request):
+    return render(request, 'base/users.html')
+
+
+def contests(request):
+    return render(request, 'base/contests.html')
