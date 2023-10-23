8e86724ac1e955b839c85f165bca3dc512377a1d
Borsh69 <uludintsevkot@gmail.com>
2023-10-23 20:18:44 +0300

    тимур я все сломал, чини

--- metophotobook/base/views.py
+++ metophotobook/base/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
-    return render(request, 'base/home.html')
+    photo = Photo.objects.filter(isopen=True)
+    popul_photo = photo.order_by('-popularity')[:5]
+    user = User.objects.all()
+    close_photo = Photo.objects.filter(isopen=False)
+    context = {'close_photo': close_photo,'photo_popul': popul_photo, 'user': user}
+    return render(request, 'base/home.html', context)
 
 def contest(request):
     return render(request, 'base/contest-details.html')
