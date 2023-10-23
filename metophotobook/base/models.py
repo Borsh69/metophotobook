05343107f87a658104f15fdcacf065427b720d03
zhabensky <67779753+cyan1dy@users.noreply.github.com>
2023-10-23 23:24:56 +0300

    Thx <3

--- metophotobook/base/models.py
+++ metophotobook/base/models.py
@@ -7,22 +7,30 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
 
+
+
+class Photo(models.Model):
+    name = models.CharField(max_length=200)
+    original = models.ImageField(upload_to='photos/%Y/', verbose_name="Original photo")
+    resize = models.ImageField(upload_to='resize/', blank=True, null=True)
+    popularity = models.IntegerField(default=0)
+    isopen = models.BooleanField(default=1)
+    photo_type = models.TextChoices('Природа', 'Научная Портрет Бытовая Другое')
+    category = models.CharField(choices=photo_type.choices, max_length=100, verbose_name="Тип фото", default='Другое')
+    def __str__(self):
+        return f"{self.name}"
+    
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=200, verbose_name="username")
     photo_avatar = models.ImageField(upload_to="images/avatars/%Y/%m/%d")
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=40)
+    photo = models.ManyToManyField(Photo, verbose_name="Фотографии пользователя")
     def __str__(self):
-        return f"{self.username}"
-
-class Photo(models.Model):
-    name = models.CharField(max_length=200)
-    original = models.ImageField(upload_to='photos/%Y/', verbose_name="Original photo")
-    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
-    def __str__(self):
-        return f"{self.name}"
+        return f"{self.username}"    
     
+
 class Album(models.Model):
     name = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -34,12 +42,12 @@ class Album(models.Model):
 
 
 @receiver(pre_save, sender=Photo)
-def generate_thumbnail(sender, instance, **kwargs):
+def generate_resize(sender, instance, **kwargs):
     if instance.original:
         image = PILImage.open(instance.original)
-        image.thumbnail((384, 216))
+        image.resize((384, 216))
         thumb_io = BytesIO()
         image.save(thumb_io, format='PNG')  
-        instance.thumbnail.save(instance.original.name, InMemoryUploadedFile(
-            thumb_io, None, f'thumbnail_{instance.original.name}', 'image/jpeg', thumb_io.tell(), None
+        instance.resize.save(instance.original.name, InMemoryUploadedFile(
+            thumb_io, None, f'resize_{instance.original.name}', 'image/jpeg', thumb_io.tell(), None
         ), save=False)
